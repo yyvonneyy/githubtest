dbe2bc8db717316a9d474f91a9e46cea7f64036a
yyvonneyy <121484394+yyvonneyy@users.noreply.github.com>
2023-03-09 12:16:31 +0900

    Update GithubTest.java
    깃허브에서 변경

--- src/githubtest/GithubTest.java
+++ src/githubtest/GithubTest.java
@@ -8,5 +8,7 @@ public class GithubTest {
 		System.out.println("GitHub Test4-직접수정");
 		System.out.println("GitHub Test5-변경");
 		System.out.println("GitHub Test6-소스트리");
+		System.out.println("GitHub Test7-깃허브변경");
+		
 	}
 }
