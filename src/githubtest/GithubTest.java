6f681b4cd5d9d217b3432f30d3a65e9689375821
yyvonneyy <121484394+yyvonneyy@users.noreply.github.com>
2023-03-09 11:41:20 +0900

    Update GithubTest.java
    git에서 수정

--- src/githubtest/GithubTest.java
+++ src/githubtest/GithubTest.java
@@ -5,5 +5,7 @@ public class GithubTest {
 		System.out.println("GitHub Test");
 		System.out.println("GitHub Test2");
 		System.out.println("GitHub Test3");
+		System.out.println("GitHub Test4-직접수정");
+		
 	}
 }
